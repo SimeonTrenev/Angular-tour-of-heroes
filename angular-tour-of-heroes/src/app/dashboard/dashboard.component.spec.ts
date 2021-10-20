65ae2dc3e06ba64c32297b5e1c5deba118cfd6b9
Simeon Trenev <65025840+SimeonTrenev@users.noreply.github.com>
2021-10-20 13:02:10 +0300

    Add tests

--- angular-tour-of-heroes/src/app/dashboard/dashboard.component.spec.ts
+++ angular-tour-of-heroes/src/app/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { HeroSearchComponent } from '../hero-search/hero-search.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let heroService;
+  let getHeroesSpy;
 
   beforeEach(async () => {
+    heroService = jasmine.createSpyObj('HeroService', ['getHeroes']);
+    getHeroesSpy = heroService.getHeroes.and.returnValue(of(HEROES));
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ 
+        DashboardComponent, 
+        HeroSearchComponent]
     })
     .compileComponents();
   });
